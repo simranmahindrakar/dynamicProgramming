c88771eceac7333bfebc0266f3a19a50ca486419
SIMRAN MAHINDRAKAR <66198982+simranmahindrakar@users.noreply.github.com>
2020-07-26 19:18:53 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,3 +6,18 @@ def fibonacci(n):
     return (value)
 
 print(fibonacci(5))
+-------------------------------------------------------------------------------
+//memoized fibo
+
+def fibonacci(n):
+    fibtable = {}
+    if n in fibtable:
+        return (fibtable[n])
+    if n==0 or n==1:
+        value = n
+    else:
+        value = fibonacci(n-1) + fibonacci(n-2)
+        fibtable[n] = value
+    return (value)
+
+print(fibonacci(5))
